63920848bbdec0b6b4e634ffd472497a4c112c66
Georg Seibt <seibt@fim.uni-passau.de>
2014-05-26 13:55:51 +0200

    progress update, less null tests

--- src/controller/mc_alg/service/MCComplete.java
+++ src/controller/mc_alg/service/MCComplete.java
@@ -16,16 +16,12 @@ public class MCComplete extends MCService {
 
             TriangleMesh mesh = new TriangleMesh();
 
-            Vertex v0 = null;
-            Vertex v1 = null;
-            Vertex v2 = null;
-            Vertex v3 = null;
-            Vertex v4 = null;
-            Vertex v5 = null;
-            Vertex v6 = null;
-            Vertex v7 = null;
             Cube cube;
             Cube[][] currentSlice;
+            Vertex v0, v1, v2, v3, v4, v5, v6, v7;
+
+            long numCubes = (data.length - 1) * (data[0].length - 1) * (data[0][0].length - 1);
+            long doneCubes = 0;
 
             for (int z = 0; z < data.length - 1; z++) {
 
@@ -47,47 +43,65 @@ public class MCComplete extends MCService {
 
                         if (x != 0) {
                             v0 = currentSlice[y][x - 1].getVertex(1);
+                        } else if (y != 0) {
+                            v0 = currentSlice[y - 1][x].getVertex(3);
+                        } else if (z != 0) {
+                            v0 = lowerSlice[y][x].getVertex(4);
+                        } else {
+                            v0 = new Vertex(x, y, z, data[z][y][x]);
+                        }
+
+                        if (y != 0) {
+                            v1 = currentSlice[y - 1][x].getVertex(2);
+                        } else if (z != 0) {
+                            v1 = lowerSlice[y][x].getVertex(5);
+                        } else {
+                            v1 = new Vertex(x + 1, y, z, data[z][y][x + 1]);
+                        }
+
+                        if (z != 0) {
+                            v2 = lowerSlice[y][x].getVertex(6);
+                        } else {
+                            v2 = new Vertex(x + 1, y + 1, z, data[z][y + 1][x + 1]);
+                        }
+
+                        if (x != 0) {
                             v3 = currentSlice[y][x - 1].getVertex(2);
+                        } else if (z != 0) {
+                            v3 = lowerSlice[y][x].getVertex(7);
+                        } else {
+                            v3 = new Vertex(x, y + 1, z, data[z][y + 1][x]);
+                        }
+
+                        if (x != 0) {
                             v4 = currentSlice[y][x - 1].getVertex(5);
+                        } else if (y != 0) {
+                            v4 = currentSlice[y - 1][x].getVertex(7);
+                        } else {
+                            v4 = new Vertex(x, y, z + 1, data[z + 1][y][x]);
+                        }
+
+                        if (y != 0) {
+                            v5 = currentSlice[y - 1][x].getVertex(6);
+                        } else {
+                            v5 = new Vertex(x + 1, y, z + 1, data[z + 1][y][x + 1]);
+                        }
+
+                        v6 = new Vertex(x + 1, y + 1, z + 1, data[z + 1][y + 1][x + 1]);
+
+                        if (x != 0) {
                             v7 = currentSlice[y][x - 1].getVertex(6);
                         } else {
                             v7 = new Vertex(x, y + 1, z + 1, data[z + 1][y + 1][x]);
                         }
 
-                        if (y != 0) {
-                            if (v0 == null) v0 = currentSlice[y - 1][x].getVertex(3);
-                                            v1 = currentSlice[y - 1][x].getVertex(2);
-                            if (v4 == null) v4 = currentSlice[y - 1][x].getVertex(7);
-                                            v5 = currentSlice[y - 1][x].getVertex(6);
-                        } else {
-                            v5 = new Vertex(x + 1, y,     z + 1, data[z + 1][y]    [x + 1]);
-                            v6 = new Vertex(x + 1, y + 1, z + 1, data[z + 1][y + 1][x + 1]);
-                        }
-
-                        if (z != 0) {
-                            if (v0 == null) v0 = lowerSlice[y][x].getVertex(4);
-                            if (v1 == null) v1 = lowerSlice[y][x].getVertex(5);
-                                            v2 = lowerSlice[y][x].getVertex(6);
-                            if (v3 == null) v3 = lowerSlice[y][x].getVertex(7);
-                        } else {
-                            if (v6 == null) v6 = new Vertex(x + 1, y + 1, z + 1, data[z + 1][y + 1][x + 1]);
-                                            v2 = new Vertex(x + 1, y + 1, z,     data[z    ][y + 1][x + 1]);
-                        }
-
-                        if (v0 == null) v0 = new Vertex(x,     y,     z,     data[z]    [y]    [x]);
-                        if (v1 == null) v1 = new Vertex(x + 1, y,     z,     data[z]    [y]    [x + 1]);
-                        if (v2 == null) v2 = new Vertex(x + 1, y + 1, z,     data[z]    [y + 1][x + 1]);
-                        if (v3 == null) v3 = new Vertex(x,     y + 1, z,     data[z]    [y + 1][x]);
-                        if (v4 == null) v4 = new Vertex(x,     y,     z + 1, data[z + 1][y]    [x]);
-                        if (v5 == null) v5 = new Vertex(x + 1, y,     z + 1, data[z + 1][y]    [x + 1]);
-                        if (v6 == null) v6 = new Vertex(x + 1, y + 1, z + 1, data[z + 1][y + 1][x + 1]);
-                        if (v7 == null) v7 = new Vertex(x,     y + 1, z + 1, data[z + 1][y + 1][x]);
-
                         cube = new Cube(v0, v1, v2, v3, v4, v5, v6, v7);
                         currentSlice[y][x] = cube;
 
-                        v0 = v1 = v2 = v3 = v4 = v5 = v6 = v7 = null;
+                        doneCubes++;
                     }
+
+                    updateProgress(doneCubes, numCubes);
                 }
             }
 
