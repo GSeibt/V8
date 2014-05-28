9d8157518854d82f765755f2768b50ea9a70aa8f
Georg Seibt <seibt@fim.uni-passau.de>
2014-05-28 14:01:11 +0200

    documentation

--- src/gui/opengl/Camera.java
+++ src/gui/opengl/Camera.java
@@ -7,6 +7,9 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.util.glu.GLU.gluLookAt;
 import static org.lwjgl.util.glu.GLU.gluPerspective;
 
+/**
+ * A <code>Camera</code> for an OpenGL scene.
+ */
 public class Camera {
 
     public static final Vector3f xAxis = new Vector3f(1, 0, 0);
@@ -28,6 +31,21 @@ public class Camera {
     private float nearClip;
     private float farClip;
 
+    /**
+     * Constructs a new <code>Camera</code> using the given parameters.
+     * The <code>Camera</code> will be placed at (0,0,0) looking along the negative z-axis.
+     * OpenGL calls that initialize the projection matrix and enable the depth test for the
+     * model-view matrix will be made.
+     *
+     * @param fov
+     *         the field of view in degrees
+     * @param aspect
+     *         the aspect ratio for the camera
+     * @param nearClip
+     *         the near clipping plane
+     * @param farClip
+     *         the far clipping plane
+     */
     public Camera(float fov, float aspect, float nearClip, float farClip) {
         this.position = new Vector3f(0, 0, 0);
         this.forward = new Vector3f(0, 0, -1);
@@ -41,7 +59,10 @@ public class Camera {
         initGLProjection();
     }
 
-    public void initGLProjection() {
+    /**
+     * Initializes the projection-and model-view matrix as noted in the constructor documentation.
+     */
+    private void initGLProjection() {
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         gluPerspective(fov, aspect, nearClip, farClip);
@@ -49,11 +70,17 @@ public class Camera {
         glEnable(GL_DEPTH_TEST);
     }
 
+    /**
+     * Polls input events and moves the camera appropriately.
+     */
     public void input() {
         keyboard();
         mouse();
     }
 
+    /**
+     * Polls mouse input.
+     */
     private void mouse() {
 
         while (Mouse.next()) {
@@ -70,54 +97,82 @@ public class Camera {
         }
     }
 
+    /**
+     * Polls keyboard input.
+     */
     private void keyboard() {
-        if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
             move(forward, 0.3f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
             move(forward, -0.3f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
             move(getLeft(), 0.3f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
             move(getRight(), 0.3f);
         }
 
-        if(Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
             pitch(0.1f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
             pitch(-0.1f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
             yaw(-0.1f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
             yaw(0.1f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
             roll(-0.1f);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_E)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
             roll(0.1f);
         }
     }
 
+    /**
+     * Makes OpenGL calls to adjust the eye position according to the camera position.
+     */
     public void useView() {
-        gluLookAt(position.getX(), position.getY(), position.getZ(), position.getX() + forward.getX(),
-                position.getY() + forward.getY(), position.getZ() + forward.getZ(), upward.getX(), upward.getY(),
-                upward.getZ());
+        Vector3f center = position.add(forward);
+
+        gluLookAt(position.getX(), position.getY(), position.getZ(), center.getX(), center.getY(), center.getZ(),
+                upward.getX(), upward.getY(), upward.getZ());
     }
 
+    /**
+     * Moves the <code>Camera</code> in the given direction by the given amount.
+     * The <code>dir</code> vector will be normalized internally.
+     *
+     * @param dir
+     *         the direction
+     * @param amount
+     *         the amount
+     */
     public void move(Vector3f dir, float amount) {
-        position = position.add(dir.mul(amount));
+        position = position.add(dir.normalized().mul(amount));
     }
 
+    /**
+     * Rotates the camera around its forward vector.
+     *
+     * @param angle
+     *         the angle in degrees by which to rotate
+     */
     public void roll(float angle) {
         upward = upward.rotate(forward, angle);
     }
 
+    /**
+     * Rotates the camera around a vector pointing to its left.
+     *
+     * @param angle
+     *         the angle in degrees by which to rotate
+     */
     public void pitch(float angle) {
         Vector3f cross = getLeft();
 
@@ -125,14 +180,30 @@ public class Camera {
         forward = forward.rotate(cross, angle);
     }
 
+    /**
+     * Rotates the camera around its upward vector.
+     *
+     * @param angle
+     *         the angle in degrees by which to rotate
+     */
     public void yaw(float angle) {
         forward = forward.rotate(upward, angle);
     }
 
+    /**
+     * Gets a normalized vector pointing to the cameras left.
+     *
+     * @return the vector pointing to the left
+     */
     public Vector3f getLeft() {
         return upward.cross(forward).normalized();
     }
 
+    /**
+     * Gets a normalized vector pointing to the cameras right.
+     *
+     * @return the vector pointing to the right
+     */
     public Vector3f getRight() {
         return forward.cross(upward).normalized();
     }
