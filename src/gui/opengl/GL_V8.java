66f80002c8bc3cf06e23c67b71ca40a6310592a6
Georg Seibt <seibt@fim.uni-passau.de>
2014-06-04 21:19:58 +0200

    Light

--- src/gui/opengl/GL_V8.java
+++ src/gui/opengl/GL_V8.java
@@ -1,11 +1,11 @@
 package gui.opengl;
 
-import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.concurrent.SynchronousQueue;
 
 import controller.mc_alg.MCRunner;
 import controller.mc_alg.Mesh;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
@@ -20,21 +20,12 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class GL_V8 {
 
-    private class MeshChange {
-
-        private FloatBuffer vertexes;
-
-        private IntBuffer indices;
-        private MeshChange(FloatBuffer vertexes, IntBuffer indices) {
-            this.vertexes = vertexes;
-            this.indices = indices;
-        }
-
-    }
     private final int DEFAULT_FOV = 70;
+
     private SynchronousQueue<Mesh> newBuffer = new SynchronousQueue<>();
     private MCRunner mcRunner;
     private Camera camera;
+
     private int vaoId;  // Vertex Array Object ID
     private int vboId;  // Vertex Buffer Object ID (Points)
     private int vboiId; // Vertex Buffer Object ID (Indices)
@@ -44,6 +35,7 @@ public class GL_V8 {
     public GL_V8(float[][][] data, float level) {
         try {
             initDisplay();
+            initGLLight();
             initGLObjects();
             initInput();
         } catch (LWJGLException e) {
@@ -56,11 +48,45 @@ public class GL_V8 {
         this.mcRunner = new MCRunner(data, level, MCRunner.Type.SLICE, this::receiveUpdate);
     }
 
+    private void initGLLight() {
+        glEnable(GL_LIGHTING);
+        glEnable(GL_COLOR_MATERIAL);
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+
+        glEnable(GL_LIGHT0);
+
+        IntBuffer position = BufferUtils.createIntBuffer(4);
+        position.put(new int[] {1, 1, 0, 0}).flip();
+
+        IntBuffer ambient = BufferUtils.createIntBuffer(4);
+        ambient.put(new int[] {0,0,0,1}).flip();
+
+        IntBuffer diffuse = BufferUtils.createIntBuffer(4);
+        diffuse.put(new int[] {1,1,1,1}).flip();
+
+        IntBuffer specular = BufferUtils.createIntBuffer(4);
+        specular.put(new int[] {1,1,1,1}).flip();
+
+        glLight(GL_LIGHT0, GL_POSITION, position);
+        glLight(GL_LIGHT0, GL_AMBIENT, ambient);
+        glLight(GL_LIGHT0, GL_DIFFUSE, diffuse);
+        glLight(GL_LIGHT0, GL_SPECULAR, specular);
+
+        IntBuffer emission = BufferUtils.createIntBuffer(4);
+        emission.put(new int[] {0, 0, 0, 1}).flip();
+
+        glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+        glMaterial(GL_FRONT_AND_BACK, GL_EMISSION, emission);
+    }
+
     private void initGLObjects() {
 
         // create a new Vertex Array Object in memory and select it
         vaoId = GL30.glGenVertexArrays();
         GL30.glBindVertexArray(vaoId);
+
+        GL20.glEnableVertexAttribArray(0);
+        GL20.glEnableVertexAttribArray(1);
 
         // create a new Vertex Buffer Object for the vertexes
         vboId = GL15.glGenBuffers();
@@ -125,11 +151,11 @@ public class GL_V8 {
             GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
             GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, change.getIndices(), GL15.GL_STATIC_DRAW);
 
-            GL15.glBindBuffer(GL15.GL_NORMAL_ARRAY_BUFFER_BINDING, vbonId);
-            GL15.glBufferData(GL15.GL_NORMAL_ARRAY_BUFFER_BINDING, change.getNormals(), GL15.GL_STATIC_DRAW);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbonId);
+            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, change.getNormals(), GL15.GL_STATIC_DRAW);
 
             // Deselect (bind to 0) the VBO
-            GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
             GL30.glBindVertexArray(0);
         }
 
@@ -147,21 +173,18 @@ public class GL_V8 {
 
         // Bind to the VAO that has all the information about the vertices
         GL30.glBindVertexArray(vaoId);
-        GL20.glEnableVertexAttribArray(0);
-        GL20.glEnableVertexAttribArray(1);
 
         // Bind to the index VBO that has all the information about the order of the vertices
         GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
 
         // Draw the vertices
         GL11.glColor3f(1f, 0, 0);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         GL11.glDrawElements(GL11.GL_TRIANGLES, indicesCount, GL11.GL_UNSIGNED_INT, 0);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
         // Put everything back to default (deselect)
         GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
         GL20.glDisableVertexAttribArray(0);
+        GL20.glDisableVertexAttribArray(1);
         GL30.glBindVertexArray(0);
     }
 
