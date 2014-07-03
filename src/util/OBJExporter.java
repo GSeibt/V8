e062052456b792435ea7caed3132b2cadb1680da
Georg Seibt <seibt@fim.uni-passau.de>
2014-07-03 09:59:40 +0200

    null checks, added an 'o Objectname' line to the .obj

--- src/util/OBJExporter.java
+++ src/util/OBJExporter.java
@@ -3,8 +3,10 @@ package util;
 import java.io.*;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.Calendar;
 
 import controller.mc_alg.Mesh;
+import gui.V8;
 
 /**
  * An exporter for the .obj file format.
@@ -13,11 +15,24 @@ public class OBJExporter {
 
     /**
      * Saves the given <code>Mesh</code> as an .obj file.
+     * If <code>saveFile</code> exists and is not a directory it will be overwritten.
+     * Neither <code>mesh</code> nor <code>saveFile</code> may be <code>null</code>.
      *
      * @param mesh the <code>Mesh</code> to be exported
      * @param saveFile the <code>File</code> to save the .obj data to
      */
     public static void export(Mesh mesh, File saveFile) {
+
+        if (mesh == null || saveFile == null) {
+            System.err.println("Neither mesh nor saveFile may be null, aborting mesh export.");
+            return;
+        }
+
+        if (saveFile.isDirectory()) {
+            System.err.println("saveFile must not be a directory, aborting mesh export.");
+            return;
+        }
+
         String vertex = "v";
         String vertexNormal = "vn";
         String vectorFormatString = "%s %s %s %s %n";
@@ -25,6 +40,8 @@ public class OBJExporter {
         String faceFormatString = "%s %2$d//%2$d %3$d//%3$d %4$d//%4$d %n";
 
         try (Writer writer = new BufferedWriter(new FileWriter(saveFile))) {
+            writer.write(String.format("# Created by %s on %tc %n%n", V8.class.getSimpleName(), Calendar.getInstance()));
+            writer.write(String.format("o %s%n", getFileName(saveFile)));
 
             FloatBuffer verts = mesh.getVertices();
             while (verts.hasRemaining()) {
@@ -49,5 +66,23 @@ public class OBJExporter {
         } catch (IOException e) {
             System.err.println("Could not write the " + saveFile.getName() + " .obj file. " + e.getMessage());
         }
+    }
+
+    /**
+     * Returns the name (without the extension) of the given file.
+     * If the filename is only an extension (e.g. '.txt') the original name will be returned.
+     *
+     * @param file the file whose name is to be extracted
+     * @return the filename
+     */
+    private static String getFileName(File file) {
+        String fileName = file.getName();
+        int dotPos = fileName.lastIndexOf('.');
+
+        if (dotPos > 0) {
+            fileName = fileName.substring(0, dotPos);
+        }
+
+        return fileName;
     }
 }
