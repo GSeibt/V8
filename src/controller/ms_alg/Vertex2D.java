a69baff223237f2f8f46c5392f1d39af73da540c
Georg Seibt <seibt@fim.uni-passau.de>
2014-07-30 23:50:29 +0200

    value equality

--- src/controller/ms_alg/Vertex2D.java
+++ src/controller/ms_alg/Vertex2D.java
@@ -28,6 +28,34 @@ public class Vertex2D implements Cloneable {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vertex2D vertex2D = (Vertex2D) o;
+
+        if (Float.compare(vertex2D.x, x) != 0) {
+            return false;
+        }
+        if (Float.compare(vertex2D.y, y) != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (x != +0.0f ? Float.floatToIntBits(x) : 0);
+        result = 31 * result + (y != +0.0f ? Float.floatToIntBits(y) : 0);
+        return result;
+    }
+
+    @Override
     protected Vertex2D clone() throws CloneNotSupportedException {
         return (Vertex2D) super.clone();
     }
