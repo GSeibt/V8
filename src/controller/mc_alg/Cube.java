fa8e366edfd39d73b74823eb4d3aaf4f5278c284
Georg Seibt <seibt@fim.uni-passau.de>
2014-09-10 21:54:10 +0200

    clarification

--- src/controller/mc_alg/Cube.java
+++ src/controller/mc_alg/Cube.java
@@ -16,6 +16,7 @@ import java.util.List;
  */
 public class Cube {
 
+    // edge indices
     public static List<Integer> bottomIndices = Arrays.asList(0, 1, 2, 3);
     public static List<Integer> topIndices = Arrays.asList(4, 5, 6, 7);
     public static List<Integer> sideIndices = Arrays.asList(8, 9, 10, 11);
